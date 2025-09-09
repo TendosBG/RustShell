61f555d4eeac8a4619efbfadbefd35391fa3b234
Tendos <maxime.delhaye28@gmail.com>
2025-09-09 23:53:19 +0200

    feat: Accepting multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::io::stdin;
 use std::process::Command;
-
+use std::io::stdout;
+use std::io::Write;
 
 fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
+    loop {
+        print!("> ");
+        stdout().flush();
+        
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
 
-    let command = input.trim();
+        let command = input.trim();
 
-    Command::new(command)
-        .spawn()
-        .unwrap();
+        let mut child = Command::new(command).spawn().unwrap();
+
+        child.wait();
+    }
 }
